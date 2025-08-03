801a153faa5eaec279d3c6062d744ac1059303d4
Rico H <hrr600@gmail.com>
2025-08-02 21:24:22 -0400

    styling updates for mobile navbar

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,24 +4,32 @@ import "../css/carousel.css";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 function Carousel({ data, autoSlide = true, interval = 3000 }) {
+  // destructured props to support carousel function
+  // data prop: passed from json file to keep UI logic separate from the content for a cleaner codebase & eventual reusability & scalability
+  // autoSlide & interval passed as props to set the auto scrolling parameters
   const [slide, setSlide] = useState(0);
   console.log(data);
 
+  //   function to control "left arrow" clicking to previous slide
   const prevSlide = () => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
   };
+
+  //   function to control "right arrow" clicking to the next slide
   const nextSlide = () => {
     setSlide(slide === data.length - 1 ? 0 : slide + 1);
   };
 
   // auto-slide show effect
   useEffect(() => {
-    if(!autoSlide) return;
+    if (!autoSlide) return;
 
+    // setInterval: web based API to set the timer to call the nextSlide() function
     const slideInterval = setInterval(() => {
-        nextSlide();
+      nextSlide();
     }, interval);
 
+    // clearInterval: web based API to clear the timer
     return () => clearInterval(slideInterval);
   }, [slide, autoSlide, interval]);
 
@@ -43,6 +51,7 @@ function Carousel({ data, autoSlide = true, interval = 3000 }) {
         onClick={nextSlide}
       />
       <span className="indicators">
+        {/* image rendering by mapping over the data to display only the image that is determined to be active by idx(the index) */}
         {data.map((_, idx) => {
           return (
             <button
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,99 @@
 // NavBar component
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 
 function NavBar() {
   // destructured assignments to extra the specific values for the currentUser & logout function
   const { currentUser, logout, loading } = useUser();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   if (loading) {
-    return <div className="nav-bar">Loading...</div>
+    return <div className="nav-bar">Loading...</div>;
   }
 
   return (
-    <nav className="navbar-container flex gap-5 mx-auto">
-      <div className="nav-bar flex gap-5 mx-auto">
-        {/* <NavLink to="/">Home</NavLink> */}
-        {/* <NavLink to="/about">About</NavLink> */}
-        {/* <NavLink to=""></NavLink> */}
+    <nav className="navbar-container">
+      {/* top bar for mobile view should always show logout and greeting when logged in */}
+      <div className="nav-top">
+        <span className="greet">
+          {currentUser ? `${currentUser?.username}, Hello There!` : ""}
+        </span>
+
+        <div className="navbar-actions">
+          {currentUser && (
+            <button
+              className="logout-btn"
+              onClick={logout}
+              style={{ background: "none" }}
+            >
+              <i className="ri-logout-circle-r-line"></i>
+              Log Out
+            </button>
+          )}
+
+          {/* hamburger */}
+          <div
+            className="menu-toggle"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            {isMenuOpen ? (
+              <button>
+                <i className="ri-close-line"></i>
+              </button>
+            ) : (
+              <button>
+                <i className="ri-align-justify"></i>
+              </button>
+            )}
+          </div>
+        </div>
       </div>
 
-      {/* conditional rendering for navbar configuration */}
-      {currentUser ? (
-        <div className="navbar">
-          <span className="greet">{currentUser?.username}, Hello There!</span>
-          <NavLink to="*">User Dashboard</NavLink>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/projects">Projects</NavLink>
-          {/* <NavLink to="/tasks">Tasks</NavLink> */}
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="*">
-            <i className="ri-settings-5-line"></i>
-          </NavLink>
-          <button
-            className="logout-btn"
-            onClick={logout}
-            style={{ background: "none" }}
-          >
-            <i className="ri-logout-circle-r-line"></i>
-            Log Out
-          </button>
-        </div>
-      ) : (
-        <div className="navbar">
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/signin">Login</NavLink>
+      <div className="navbar desktop-nav">
+        {/* conditional rendering for navbar configuration */}
+        {currentUser ? (
+          <div className="navbar">
+            <NavLink to="*">User Dashboard</NavLink>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/projects">Projects</NavLink>
+            {/* <NavLink to="/tasks">Tasks</NavLink> */}
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="*">
+              <i className="ri-settings-5-line"></i>
+            </NavLink>
+          </div>
+        ) : (
+          <div className="navbar">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/signin">Login</NavLink>
+          </div>
+        )}
+      </div>
+
+      {/* mobile nav */}
+      {isMenuOpen && (
+        <div className="mobile-nav">
+          {currentUser ? (
+            // onClick function set for each NavLink in mobile menu to help control responsiveness & precision
+          <div className="navbar">
+            <NavLink to="*" onClick={() => setIsMenuOpen(false)}>User Dashboard</NavLink>
+            <NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+            <NavLink to="/projects" onClick={() => setIsMenuOpen(false)}>Projects</NavLink>
+            {/* <NavLink to="/tasks">Tasks</NavLink> */}
+            <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>About</NavLink>
+            <NavLink to="*" onClick={() => setIsMenuOpen(false)}>
+              <i className="ri-settings-5-line"></i>
+            </NavLink>
+          </div>
+        ) : (
+          <div className="navbar">
+            <NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+            <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>About</NavLink>
+            <NavLink to="/signin" onClick={() => setIsMenuOpen(false)}>Login</NavLink>
+          </div>
+        )}
         </div>
       )}
     </nav>
